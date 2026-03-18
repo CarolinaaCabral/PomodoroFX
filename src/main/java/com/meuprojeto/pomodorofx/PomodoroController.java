fa7d939259c8f1e93d5835889a3d72f88b8ac38b
CarolinaaCabral <carolinacabral@outlook.com.br>
2026-03-18 20:49:19 -0300

    feat: estrutura base do pomodoro

---
+++ src/main/java/com/meuprojeto/pomodorofx/PomodoroController.java
@@ -0,0 +1,75 @@
+package com.meuprojeto.pomodorofx;
+
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.util.Duration;
+
+public class PomodoroController {
+
+    private int segundosRestantes = 1500;
+    private Timeline pomodoroTimer;
+
+    @FXML
+    private Label timerLabel;
+
+    @FXML
+    private Button startButton;
+
+    @FXML
+    private Button resetButton;
+
+    @FXML
+    public void initialize(){
+        pomodoroTimer = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+            if (segundosRestantes > 0) {
+                segundosRestantes --;
+
+                int minutos = segundosRestantes / 60;
+                int segundos = segundosRestantes % 60;
+                timerLabel.setText(String.format("%02d:%02d", minutos, segundos));
+            } else  {
+                pomodoroTimer.stop();
+                timerLabel.setText("Tempo finalizado");
+                startButton.setText("Reiniciar");
+                segundosRestantes = 1500;
+
+                startButton.setDisable(false);
+                resetButton.setDisable(true);
+            }
+
+        }));
+        pomodoroTimer.setCycleCount(Timeline.INDEFINITE);
+        resetButton.setDisable(true);
+    }
+
+    @FXML
+    private void handleStart() {
+
+        if (pomodoroTimer.getStatus() == Animation.Status.RUNNING) {
+
+            pomodoroTimer.stop();
+            startButton.setText("Iniciar");
+            resetButton.setDisable(false);
+
+        } else {
+            pomodoroTimer.play();
+            startButton.setText("Pausar");
+            resetButton.setDisable(false);
+        }
+    }
+
+    @FXML
+    private void handleReset() {
+        pomodoroTimer.stop();
+        segundosRestantes = 1500;
+        timerLabel.setText("25:00");
+        startButton.setText("Iniciar");
+
+        resetButton.setDisable(true);
+        startButton.setDisable(false);
+    }
+}
