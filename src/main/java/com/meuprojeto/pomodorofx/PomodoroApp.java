0c86b6bea317af7ce05fa4a0124c20ba9c602e46
CarolinaaCabral <carolinacabral@outlook.com.br>
2026-03-19 22:31:44 -0300

    style: implementação css ao pomodoro

--- src/main/java/com/meuprojeto/pomodorofx/PomodoroApp.java
+++ src/main/java/com/meuprojeto/pomodorofx/PomodoroApp.java
@@ -3,7 +3,9 @@ package com.meuprojeto.pomodorofx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -11,9 +13,12 @@ public class PomodoroApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(PomodoroApp.class.getResource("pomodoro-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 500, 350);
+        scene.getStylesheets().add(PomodoroApp.class.getResource("style.css").toExternalForm());
+        stage.setTitle("Pomodoro");
         stage.setScene(scene);
+        stage.getIcons().add(new Image(PomodoroApp.class.getResourceAsStream("icone.png")));
         stage.show();
+
     }
 }
